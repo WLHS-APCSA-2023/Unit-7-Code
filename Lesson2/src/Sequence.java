a224c41004e863eab5c14d5b93ff80379e4644b8
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-15 14:26:26 -0800

    more added

--- Lesson2/src/Sequence.java
+++ Lesson2/src/Sequence.java
@@ -3,8 +3,17 @@ import java.util.ArrayList;
 public class Sequence {
 
     private ArrayList<Integer> values;
+    
     public Sequence(){
         values = new ArrayList<Integer>();
+    }
+
+    public void add(int n){
+        values.add(n);
+    }
+
+    public String toString(){
+        return values.toString();
     }
 
 
