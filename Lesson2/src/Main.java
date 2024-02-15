6945f1c9247c7d3c9c4db9139a0d3eeb7cfecf0e
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-15 14:35:57 -0800

    tests added

--- Lesson2/src/Main.java
+++ Lesson2/src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Sequence firstSequence = new Sequence();
+        firstSequence.add(1);
+        firstSequence.add(4);
+        firstSequence.add(9);
+        firstSequence.add(16);
+        System.out.println("First before: ");
+        System.out.println(firstSequence.toString());
+
+        Sequence secondSequence = new Sequence();
+        secondSequence.add(9);
+        secondSequence.add(7);
+        secondSequence.add(4);
+        secondSequence.add(9);
+        secondSequence.add(11);
+        System.out.println("Second before: ");
+        System.out.println(secondSequence.toString());
+
+        Sequence combinbedSequence = firstSequence.append(secondSequence);
+        System.out.println("Appended: ");
+        System.out.println(combinbedSequence.toString());
+        System.out.println("Expected: [1,4,9,16,9,7,4,9,11]");
+
+        System.out.println("First after: ");
+        System.out.println(firstSequence.toString());
+
+        System.out.println("Second after: ");
+        System.out.println(secondSequence.toString());
+
     }
 }
