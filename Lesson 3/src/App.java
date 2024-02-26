11fb3b31024772dcf4f44b858b7bafc200dec020
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-26 14:07:02 -0800

    first part done

--- Lesson 3/src/App.java
+++ Lesson 3/src/App.java
@@ -1,12 +1,26 @@
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
          // Make an ArrayList of Integers with the following
          // values: 12,-3,18,27,-24,-5,47,33, 14, 0
+        int[] nums = {12,-3,18,27,-24,-5,47,33, 14, 0};
+        ArrayList<Integer> flexibleList = new ArrayList<Integer>();
 
+        for(int num:nums){
+            flexibleList.add(num);
+        }
          // Traverse the list and print all values
-
+         for(int i=0; i<flexibleList.size();i++){
+            System.out.println(flexibleList.get(i));
+         }
+         System.out.println("\n");
          // Traverse the list and print all evens
-
+         for(int i=0; i<flexibleList.size();i++){
+            int current = flexibleList.get(i);
+            if(current % 2 == 0){
+                System.out.println(current);
+            }
+         }
          // Remove all even numbers from the list (the list
          // is mutated)
 
