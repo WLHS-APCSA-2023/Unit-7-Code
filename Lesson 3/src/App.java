cd2c271fdd9a8f50307a9bdfc2f87498ed778bfa
smithbwlhs <smithb@wlwv.k12.or.us>
2024-02-26 13:56:56 -0800

    new lesson

---
+++ Lesson 3/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         // Make an ArrayList of Integers with the following
+         // values: 12,-3,18,27,-24,-5,47,33, 14, 0
+
+         // Traverse the list and print all values
+
+         // Traverse the list and print all evens
+
+         // Remove all even numbers from the list (the list
+         // is mutated)
+
+         // Print this even-less list
+    }
+}
